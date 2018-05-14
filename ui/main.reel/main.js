a9f5169591bce96cd178b2291c841d51695c90fb
Thomas Jaede <thomas.jaede@kaazing.com>
2018-05-14 16:53:46 -0700

    Correct moduleId of message-service-reference

--- ui/main.reel/main.js
+++ ui/main.reel/main.js
@@ -7,7 +7,7 @@ var deserialize = require('montage/core/serialization/deserializer/montage-deser
 
 //var mainService = require("data/main.mjson").montageObject;
 // var mainService = require("data/main-remote.mjson").montageObject;
-var mainService = require("data/services/remote-service.mjson").montageObject;
+var mainService = require("data/service/remote-service.mjson").montageObject;
 
 var Message = require("data/descriptors/message.mjson").montageObject;
 var Person = require("data/descriptors/person.mjson").montageObject;
@@ -63,7 +63,6 @@ exports.Main = Component.specialize(/** @lends Main# */ {
                     var myMsg = mainService.createDataObject(dataType);
                     myMsg.subject = "RE: You've got mail";
                     mainService.saveDataObject(myMsg).then(function () {
-
                         assert('saveDataObject.created', typeof myMsg.created !== 'undefined', myMsg);
                         assert('saveDataObject.updated', typeof myMsg.updated === 'undefined', myMsg);
                         myMsg.text = "Add missing text";
